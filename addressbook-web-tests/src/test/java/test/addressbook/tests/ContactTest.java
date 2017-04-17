e2f976a1b619f650ba9f739145c7a2f1be4081ad
alpheratz404 <alpheratz404@gmail.com>
2017-04-17 11:54:32 +0300

    Задание 8.1 (дефолтные значения полей для групп и контактов).

--- addressbook-web-tests/src/test/java/test/addressbook/tests/ContactTest.java
+++ addressbook-web-tests/src/test/java/test/addressbook/tests/ContactTest.java
@@ -8,7 +8,7 @@ public class ContactTest extends TestBase {
     @Test
     public void testContact() {
         app.getContactHelper().addNewContact();
-        app.getContactHelper().enterData(new ContactData("Masha", "Ivanova", "79773456757", "gav@gav.ru", "city"));
+        app.getContactHelper().enterData(new ContactData("Masha", "Ivanova", "79773456757", null, null));
         app.getContactHelper().submitContact();
         app.getContactHelper().returnHome();
     }
